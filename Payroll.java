959b25f72b32db20ebcbbf73279c82eeb871288f
TotalEpicness <42422230+PhantomEpicness@users.noreply.github.com>
2023-09-16 20:58:45 -1000

    Add files via upload

--- Payroll.java
+++ Payroll.java
@@ -1,3 +1,9 @@
+
+/*
+* @author Donald Wyand
+* @date 9/14/2023
+* ICS 211 Payroll assignment
+*/
 import java.util.Scanner;
 import java.util.InputMismatchException;
 import java.util.*;
@@ -8,7 +14,7 @@ class Payroll {
         Scanner input = new Scanner(System.in);
         int min = 5;
         int max = 20;
-        int[] hours;
+        int[][] payroll;
         System.out.println("Enter the number of employees: (" + min + " to " + max + ")");
         int employees = input.nextInt();
         if((employees < min) || (employees > max)) {
@@ -16,12 +22,16 @@ class Payroll {
             return;
         }
         else{
-            System.out.println("Generating hour for " + employees + " employees...");
-            /// math.random()
+            System.out.println("Generating hours for " + employees + " employees...");
+            for(int i = 0; i < employees; i++) {
+                int hours = (int)(Math.random() * 12);
+                System.out.println(hours);
+            }
         }
         for (int i = 0;i <= employees; i++) {
             
         }
     
     }
+
 }
